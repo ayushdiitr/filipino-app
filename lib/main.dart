ce09307e7f59a1e1c107526c1df7dd19bab4b402
Ayush Dhiman <ayush@ezaix.com>
2024-09-06 02:05:02 +0530

    dob selector

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:testapp/src/ui/screens/splash/basicInfo.dart';
 import 'package:testapp/src/ui/screens/splash/drink.dart';
 import 'package:testapp/src/ui/screens/splash/interest.dart';
 import 'package:testapp/src/ui/screens/splash/main.dart';
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: BasicInfo(userId: 12),
     );
   }
 }
--- lib/src/ui/screens/splash/basicInfo.dart
+++ lib/src/ui/screens/splash/basicInfo.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:testapp/src/ui/screens/splash/genderScreen.dart';
+import 'package:flutter_holo_date_picker/flutter_holo_date_picker.dart';
 
 class BasicInfo extends StatefulWidget {
   final int userId;
@@ -23,7 +23,6 @@ class _BasicInfoState extends State<BasicInfo> {
   Future<void> saveBasicInfo() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     final String firstName = firstNameController.text.trim();
-    print("$selectedDate");
 
     if (firstName.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -42,7 +41,7 @@ class _BasicInfoState extends State<BasicInfo> {
     await prefs.setString('firstName', firstName);
     await prefs.setString('lastName', lastNameController.text.trim());
     await prefs.setString('occupation', occupationController.text.trim());
-    await prefs.setString('age', selectedDate!);
+    await prefs.setString('dob', selectedDate!);
     await prefs.setInt('userId', widget.userId);
 
     Navigator.push(
@@ -50,43 +49,23 @@ class _BasicInfoState extends State<BasicInfo> {
       MaterialPageRoute(builder: (context) => const Genderscreen()),
     );
   }
-  void showCupertinoDatePicker(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext builder) {
-        return Container(
-          height: MediaQuery.of(context).copyWith().size.height * 0.35,
-          child: Column(
-            children: [
-              Container(
-                alignment: Alignment.centerRight,
-                padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text('Done'),
-                ),
-              ),
-              SizedBox(
-                height: 200,
-                child: CupertinoDatePicker(
-                  mode: CupertinoDatePickerMode.date,
-                  initialDateTime: DateTime(2000, 1, 1),
-                  minimumYear: 1990,
-                  maximumYear: DateTime.now().year,
-                  onDateTimeChanged: (DateTime newDateTime) {
-                    setState(() {
-                      selectedDate = DateFormat('dd-MM-yyyy').format(newDateTime);
-                    });
-                  },
-                ),
-              ),
-            ],
-          ),
-        );
-      },
+
+  void showHoloDatePicker(BuildContext context) async {
+    var datePicked = await DatePicker.showSimpleDatePicker(
+      context,
+      initialDate: DateTime(2000, 1, 1),
+      firstDate: DateTime(1990),
+      lastDate: DateTime(DateTime.now().year),
+      dateFormat: "dd-MMMM-yyyy",
+      locale: DateTimePickerLocale.en_us,
+      looping: true,
     );
+
+    if (datePicked != null) {
+      setState(() {
+        selectedDate = DateFormat('dd-MM-yyyy').format(datePicked);
+      });
+    }
   }
 
   @override
@@ -94,21 +73,22 @@ class _BasicInfoState extends State<BasicInfo> {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       body: Stack(
-          children: [
-      SingleChildScrollView(
-      child: Padding(
-      padding: const EdgeInsets.only(bottom: 100.0),
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          const SizedBox(height: 40),
-      const Padding(
-        padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
-        child: Center(
-          child: LinearProgressIndicator(
-              value: 0.5,
-              color: Colors.black,
-              backgroundColor: Color(0xFFEEEEEE),
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 100.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 40),
+                  const Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+                    child: Center(
+                      child: LinearProgressIndicator(
+                        value: 0.5,
+                        color: Colors.black,
+                        backgroundColor: Color(0xFFEEEEEE),
                       ),
                     ),
                   ),
@@ -116,7 +96,7 @@ class _BasicInfoState extends State<BasicInfo> {
                     padding: const EdgeInsets.only(left: 16.0, top: 32.0),
                     child: Text.rich(
                       TextSpan(
-                        text: 'Enter your ', // Default text style
+                        text: 'Enter your ',
                         style: const TextStyle(
                           fontFamily: 'NoirPro',
                           fontWeight: FontWeight.w500,
@@ -140,7 +120,8 @@ class _BasicInfoState extends State<BasicInfo> {
                     padding: EdgeInsets.only(left: 16.0),
                     child: Text.rich(
                       TextSpan(
-                        text: "Share your basic info to tailor your SkyBuddy experience and connect with like-minded travelers on your upcoming journey",
+                        text:
+                            "Share your basic info to tailor your SkyBuddy experience and connect with like-minded travelers on your upcoming journey",
                         style: TextStyle(
                           fontFamily: 'NoirPro',
                           fontWeight: FontWeight.w300,
@@ -155,105 +136,95 @@ class _BasicInfoState extends State<BasicInfo> {
                       children: [
                         TextField(
                           cursorColor: Colors.black,
-                          controller: firstNameController, // Added controller
-
-                          style: const TextStyle(color: Colors.black), // Text color when focused
+                          controller: firstNameController,
+                          style: const TextStyle(color: Colors.black),
                           decoration: const InputDecoration(
                             labelText: 'First Name',
-                            labelStyle:  TextStyle(color: Colors.black), // Label color when not focused
-                            border:  OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.grey),
-                            ),
-                            focusColor: Colors.black,
-                            enabledBorder:  OutlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xFFCCCCCC)), // Border color when not focused
-                            ),
-                            focusedBorder:  OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black), // Border color when focused
-                            ),
-                          ),
-                          onChanged: (value) {
-                            print(value);
-                          },
-                        ),
-                        const SizedBox(
-                            height: 16.0), // Add space between the text fields
-                        TextField(
-                          cursorColor: Colors.black,
-                          controller: lastNameController, // Added controller
-                          style: const TextStyle(color: Colors.black), // Text color when focused
-                          decoration: const InputDecoration(
-                            labelText: 'Last Name (Optional)',
-                            labelStyle:  TextStyle(color: Colors.black), // Label color when not focused
+                            labelStyle: TextStyle(color: Colors.black),
                             border: OutlineInputBorder(
                               borderSide: BorderSide(color: Colors.grey),
                             ),
-                            enabledBorder:  OutlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xFFCCCCCC)), // Border color when not focused
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFCCCCCC)),
                             ),
-                            focusedBorder:  OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black), // Border color when focused
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black),
                             ),
                           ),
-                          onChanged: (value) {
-                            print(value);
-                          },
                         ),
-                        const SizedBox(
-                            height: 16.0), // Add space between the text fields
+                        const SizedBox(height: 16.0),
                         TextField(
                           cursorColor: Colors.black,
-                          controller: occupationController, // Added controller
-                          style: const TextStyle(color: Colors.black), // Text color when focused
+                          controller: lastNameController,
+                          style: const TextStyle(color: Colors.black),
                           decoration: const InputDecoration(
-                            labelText: 'Occupation (Optional)',
-                            labelStyle:  TextStyle(color: Colors.black), // Label color when not focused
-                            border:  OutlineInputBorder(
+                            labelText: 'Last Name (Optional)',
+                            labelStyle: TextStyle(color: Colors.black),
+                            border: OutlineInputBorder(
                               borderSide: BorderSide(color: Colors.grey),
                             ),
-                            enabledBorder:  OutlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xFFCCCCCC)), // Border color when not focused
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFCCCCCC)),
                             ),
-                            focusedBorder:  OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black), // Border color when focused
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black),
                             ),
                           ),
-                          onChanged: (value) {
-                            print(value);
-                          },
                         ),
-                        const SizedBox(
-                            height: 16.0), // Add space between the text fields
+                        const SizedBox(height: 16.0),
+                        TextField(
+                          cursorColor: Colors.black,
+                          controller: occupationController,
+                          style: const TextStyle(color: Colors.black),
+                          decoration: const InputDecoration(
+                            labelText: 'Occupation (Optional)',
+                            labelStyle: TextStyle(color: Colors.black),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFCCCCCC)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16.0),
                         GestureDetector(
-                          onTap: () => showCupertinoDatePicker(context),
+                          onTap: () => showHoloDatePicker(context),
                           child: Container(
                             padding: const EdgeInsets.symmetric(vertical: 16.0),
                             decoration: BoxDecoration(
                               border: Border.all(
-                                color: selectedDate != null ? Colors.black: Colors.grey,
+                                color: selectedDate != null
+                                    ? Colors.black
+                                    : Colors.grey,
                                 width: 1.0,
                               ),
-                              // borderRadius: BorderRadius.circular(8.0),
                             ),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 8.0),
-                                    child: Text(
-                                      selectedDate != null ?
-                                      "$selectedDate"
-                                          : "Select Date of Birth",
-                                      style: TextStyle(
-                                        color: selectedDate != null
-                                            ? Colors.black
-                                            : Colors.grey,
-                                        fontSize: 16.0,
-                                      ),
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 0, horizontal: 8.0),
+                                  child: Text(
+                                    selectedDate != null
+                                        ? "$selectedDate"
+                                        : "Select Date of Birth",
+                                    style: TextStyle(
+                                      color: selectedDate != null
+                                          ? Colors.black
+                                          : Colors.grey,
+                                      fontSize: 16.0,
                                     ),
+                                  ),
                                 ),
-                                const Padding(padding: EdgeInsets.only(right:4.0),
-                                  child: Icon(Icons.calendar_today, color: Colors.black),
+                                const Padding(
+                                  padding: EdgeInsets.only(right: 4.0),
+                                  child: Icon(Icons.calendar_today,
+                                      color: Colors.black),
                                 ),
                               ],
                             ),
@@ -266,8 +237,6 @@ class _BasicInfoState extends State<BasicInfo> {
               ),
             ),
           ),
-
-          // The button is placed at the bottom of the screen.
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
@@ -276,7 +245,7 @@ class _BasicInfoState extends State<BasicInfo> {
               child: FractionallySizedBox(
                 widthFactor: 0.9,
                 child: ElevatedButton(
-                  onPressed: () async{
+                  onPressed: () async {
                     await saveBasicInfo();
                   },
                   style: ElevatedButton.styleFrom(
@@ -293,13 +262,7 @@ class _BasicInfoState extends State<BasicInfo> {
                       borderRadius: BorderRadius.circular(8.0),
                     ),
                   ),
-                  child: const Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text('Next'),
-                    ],
-                  ),
+                  child: const Text('Next'),
                 ),
               ),
             ),
