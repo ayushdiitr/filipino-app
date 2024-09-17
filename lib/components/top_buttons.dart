2bc550dce12b62480e1e29bbe096849b7c529b0b
Ayush Dhiman <ayush@ezaix.com>
2024-09-17 21:59:28 +0530

    homepage code

---
+++ lib/components/top_buttons.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class HeaderButtons extends StatelessWidget {
+  final bool isVerified;
+
+  const HeaderButtons({
+    super.key,
+    this.isVerified = false, // Assuming verified is optional
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                backgroundColor: 1 > 0 ? Colors.black : Colors.white,
+                side: const BorderSide(
+                  color: Colors.black,
+                  width: 1,
+                ),
+              ),
+              child: const Text(
+                'All',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'NoirPro',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400),
+              ),
+            ),
+            const SizedBox(width: 10), // Spacing between buttons
+            OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                side: const BorderSide(
+                  color: Colors.black,
+                  width: 1,
+                ),
+              ),
+              child: const Text(
+                'Top Matches',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'NoirPro',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400),
+              ),
+            ),
+            const SizedBox(width: 10), // Spacing between buttons
+            OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                side: const BorderSide(
+                  color: Colors.black,
+                  width: 1,
+                ),
+              ),
+              child: const Text(
+                'Filters',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'NoirPro',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/src/ui/homepage/main.dart
+++ lib/src/ui/homepage/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:testapp/components/details_card.dart';
 import 'package:testapp/components/photo.dart';
 import 'package:testapp/components/post_list.dart';
 import 'package:testapp/components/profile_header.dart';
+import 'package:testapp/components/top_buttons.dart';
 import 'package:testapp/src/ui/splash/screen2.dart';
 import 'package:testapp/components/about_me.dart';
 import 'package:testapp/components/prompt_text.dart';
-import 'package:testapp/components/button.dart.';
-//import 'package:sign_in_button/sign_in_button.dart';
-//import 'package:testapp/src/ui/screens/splash/screen2.dart';
-//import 'package:testapp/src/ui/screens/splash/interest.dart';
-//import 'package:testapp/src/ui/screens/splash/screen2.dart';
-//import 'package:testapp/src/ui/screens/splash/lifestyle.dart';
+import 'package:testapp/components/button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -25,58 +20,62 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      backgroundColor: const Color.fromRGBO(245, 245, 245, 1),
-      body: const SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
-          child: Column(
-            children: [
-              ProfileHeader(
-                name: 'Anshika',
-                bio: 'SWE',
-                isVerified: true,
+      backgroundColor: Color.fromRGBO(245, 245, 245, 1),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverList(
+              delegate:
+                  SliverChildBuilderDelegate((BuildContext context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+              child: HeaderButtons(),
+            );
+          }, childCount: 1)),
+          const SliverAppBar(
+            pinned: true,
+            floating: true,
+            // stretchTriggerOffset: 50,
+            toolbarHeight: 72,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+                child: ProfileHeader(
+                  name: 'Anshika',
+                  bio: 'SWE',
+                  isVerified: true,
+                ),
               ),
-              SizedBox(height: 24),
-              SquareImageWithButton(
-                imgUrl:
-                    "https://images.unsplash.com/photo-1472586662442-3eec04b9dbda?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-              ),
-              SizedBox(height: 16),
-              CardRow(),
-              HomeScreen(),
-              SizedBox(height: 16),
-              SquareImageWithButton(
-                  imgUrl:
-                      "https://images.unsplash.com/photo-1472586662442-3eec04b9dbda?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-              SizedBox(height: 16),
-              PromptTextScreen(
-                promptTitle: 'Prompt 1',
-                promptDesc:
-                    'Lorem ipsum dolor sit amet consectetur. Vulputate molestie eget eget tellus ipsum adipiscing et congue vulputate.',
-              ),
-              SquareImageWithButton(
-                  imgUrl:
-                      "https://images.unsplash.com/photo-1472586662442-3eec04b9dbda?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-              SizedBox(height: 16),
-              PromptTextScreen(
-                promptTitle: 'Prompt 1',
-                promptDesc:
-                    'Lorem ipsum dolor sit amet consectetur. Vulputate molestie eget eget tellus ipsum adipiscing et congue vulputate.',
-              ),
-              SizedBox(height: 16),
-              PromptTextScreen(
-                promptTitle: 'LANGUAGES I KNOW',
-                hasButton: true,
-                // promptDesc:
-                //     'Lorem ipsum dolor sit amet consectetur. Vulputate molestie eget eget tellus ipsum adipiscing et congue vulputate.',
-              ),
-              CloseButtonScreen(),
-            ],
+            ),
           ),
-        ),
+          SliverList(delegate:
+              SliverChildBuilderDelegate((BuildContext context, int index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(
+                  vertical: 16.0,
+                  horizontal: 12.0), // Restores original padding
+              child: Column(
+                children: [
+                  // SizedBox(height: 24),
+                  SquareImageWithButton(
+                    imgUrl:
+                        "https://images.unsplash.com/photo-1472586662442-3eec04b9dbda?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                  ),
+                  SizedBox(height: 16),
+                  CardRow(),
+                  HomeScreen(),
+                  SizedBox(height: 16),
+                  PromptTextScreen(
+                    promptTitle: 'You have reached the end of the page',
+                    promptDesc: 'Would you like to see more?',
+                  ),
+                  SizedBox(height: 20),
+                  CloseButtonScreen(),
+                  SizedBox(height: 20),
+                ],
+              ),
+            );
+          }))
+        ],
       ),
     );
   }
