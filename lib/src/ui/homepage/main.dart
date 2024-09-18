c19c718ab5163732f99b63fc855b9c6a5164027b
Anshika <anshikarathour04@gmail.com>
2024-09-19 02:05:18 +0530

    verification

--- lib/src/ui/homepage/main.dart
+++ lib/src/ui/homepage/main.dart
@@ -8,6 +8,7 @@ import 'package:testapp/components/about_me.dart';
 import 'package:testapp/components/prompt_text.dart';
 import 'package:testapp/components/button.dart';
 import 'package:testapp/components/bottom_menu.dart';
+import 'package:testapp/components/verified.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -111,7 +112,7 @@ class _HomePageState extends State<HomePage> {
           SliverList(
               delegate:
                   SliverChildBuilderDelegate((BuildContext context, int index) {
-            return const Padding(
+            return  Padding(
               padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
               child: Column(
                 children: [
@@ -142,7 +143,8 @@ class _HomePageState extends State<HomePage> {
                   // ),
                   // SizedBox(height: 16),
 
-                  SquareImagePopup(imgUrl: "https://images.unsplash.com/photo-1472586662442-3eec04b9dbda?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",),
+                  //SquareImagePopup(imgUrl: "https://images.unsplash.com/photo-1472586662442-3eec04b9dbda?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",),
+                  Verification(),
                   // PromptTextScreen(
                   //   promptTitle: 'Prompt 1',
                   //   promptDesc:
