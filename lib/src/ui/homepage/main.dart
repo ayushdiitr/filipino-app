0f9d1eea8c79b5ac355df9906165069714eddc2b
Ayush Dhiman <ayushd175@gmail.com>
2024-09-28 22:21:38 +0530

    added animation

--- lib/src/ui/homepage/main.dart
+++ lib/src/ui/homepage/main.dart
@@ -22,15 +22,54 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+class User {
+  final String name;
+  final String bio;
+  final String imgUrl;
+  final bool isVerified;
+  final String? description;
+
+  User({
+    required this.name,
+    required this.bio,
+    required this.imgUrl,
+    this.isVerified = false,
+    required this.description,
+  });
+}
+
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
   late ScrollController _scrollController;
-  late bool hasScrolled = false;
+  bool hasScrolled = false;
   Color _appBackgroundColor = const Color.fromRGBO(245, 245, 245, 1);
   bool _showHeart = false;
   late AnimationController _animationController;
   late Animation<double> _scaleAnimation;
   late Animation<double> _fadeAnimation;
+
+  // List of users
+  final List<User> _users = [
+    User(
+      name: 'Anshika',
+      bio: 'SWE',
+      imgUrl:
+          "https://images.unsplash.com/photo-1472586662442-3eec04b9dbda?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVerified: true,
+      description: 'I am a software engineer',
+    ),
+    User(
+      name: 'John Doe',
+      bio: 'Graphic Designer',
+      imgUrl:
+          "https://images.unsplash.com/photo-1556740772-1a741367b93e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVerified: false,
+      description: 'I am a graphic designer',
+    ),
+    // Add more users as needed
+  ];
+
+  int _currentUserIndex = 0;
 
   @override
   void initState() {
@@ -78,23 +117,47 @@ class _HomePageState extends State<HomePage>
         curve: const Interval(0.5, 1.0, curve: Curves.easeOut),
       ),
     );
+
+    // Add the listener once
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          _showHeart = false; // Hide heart
+          _currentUserIndex++; // Move to the next user
+          if (_currentUserIndex >= _users.length) {
+            _currentUserIndex =
+                0; // Loop back to the first user or handle as needed
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('No more profiles')),
+            );
+          }
+        });
+        _animationController.reset();
+      }
+    });
   }
 
   void _triggerHeartAnimation() {
+    if (_users.isEmpty) return;
+
     setState(() {
       _showHeart = true; // Show heart
     });
 
     // Start animation
     _animationController.forward(from: 0);
+  }
 
-    // Hide heart after the animation is complete
-    _animationController.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        setState(() {
-          _showHeart = false; // Hide heart
-        });
-        _animationController.reset();
+  void _triggerDislikeAction() {
+    if (_users.isEmpty) return;
+
+    setState(() {
+      _currentUserIndex++;
+      if (_currentUserIndex >= _users.length) {
+        _currentUserIndex = 0; // Loop back or handle accordingly
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('No more profiles')),
+        );
       }
     });
   }
@@ -109,8 +172,18 @@ class _HomePageState extends State<HomePage>
 
   @override
   Widget build(BuildContext context) {
+    // Ensure there are users to display
+    if (_users.isEmpty) {
+      return Scaffold(
+        body: Center(child: Text('No more profiles')),
+      );
+    }
+
+    // Get the current user
+    final currentUser = _users[_currentUserIndex];
+
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(245, 245, 245, 1),
+      backgroundColor: _appBackgroundColor,
       body: Stack(
         alignment: Alignment.center,
         children: [
@@ -120,9 +193,9 @@ class _HomePageState extends State<HomePage>
               SliverList(
                 delegate: SliverChildBuilderDelegate(
                   (BuildContext context, index) {
-                    return const Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 16),
                       child: HeaderButtons(),
                     );
                   },
@@ -147,9 +220,9 @@ class _HomePageState extends State<HomePage>
                           : const EdgeInsets.symmetric(
                               horizontal: 16.0, vertical: 10),
                       child: ProfileHeader(
-                        name: 'Anshika',
-                        bio: 'SWE',
-                        isVerified: true,
+                        name: currentUser.name,
+                        bio: currentUser.bio,
+                        isVerified: currentUser.isVerified,
                         hasScrolled: hasScrolled,
                       ),
                     ),
@@ -159,74 +232,73 @@ class _HomePageState extends State<HomePage>
               SliverList(
                 delegate: SliverChildBuilderDelegate(
                     (BuildContext context, int index) {
-                  return Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 6.0, horizontal: 12.0),
-                        child: Column(
-                          children: [
-                            const SquareImageWithButton(
-                              imgUrl:
-                                  "https://images.unsplash.com/photo-1472586662442-3eec04b9dbda?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                            ),
-                            const SizedBox(height: 16),
-                            const CardRow(),
-                            const HomeScreen(),
-                            const SizedBox(height: 16),
-                            const SquareImageWithButton(
-                              imgUrl:
-                                  "https://images.unsplash.com/photo-1472586662442-3eec04b9dbda?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                            ),
-                            const SizedBox(height: 16),
-                            const PromptTextScreen(
-                              promptTitle: 'Prompt 1',
-                              promptDesc:
-                                  'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-                            ),
-                            const SizedBox(height: 16),
-                            const SquareImageWithButton(
-                              imgUrl:
-                                  "https://images.unsplash.com/photo-1472586662442-3eec04b9dbda?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                            ),
-                            const SizedBox(height: 16),
-                            const PromptTextScreen(
-                              promptTitle: 'Prompt 1',
-                              promptDesc:
-                                  'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-                            ),
-                            const SizedBox(height: 16),
-                            Verification(),
-                            const SizedBox(height: 16),
-                            ProfilePrompts(),
-                            const PromptTextScreen(
-                              promptTitle: 'Laptop',
-                              hasButton: true,
-                            ),
-                            const SizedBox(height: 16),
-                          ],
+                  return SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 6.0, horizontal: 12.0),
+                          child: Column(
+                            children: [
+                              SquareImageWithButton(
+                                imgUrl: currentUser.imgUrl,
+                              ),
+                              const SizedBox(height: 16),
+                              const CardRow(),
+                              const HomeScreen(),
+                              const SizedBox(height: 16),
+                              SquareImageWithButton(
+                                imgUrl: currentUser.imgUrl,
+                              ),
+                              const SizedBox(height: 16),
+                              const PromptTextScreen(
+                                promptTitle: 'Prompt 1',
+                                promptDesc:
+                                    'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+                              ),
+                              const SizedBox(height: 16),
+                              SquareImageWithButton(
+                                imgUrl: currentUser.imgUrl,
+                              ),
+                              const SizedBox(height: 16),
+                              const PromptTextScreen(
+                                promptTitle: 'Prompt 1',
+                                promptDesc:
+                                    'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+                              ),
+                              const SizedBox(height: 16),
+                              Verification(),
+                              const SizedBox(height: 16),
+                              ProfilePrompts(),
+                              const PromptTextScreen(
+                                promptTitle: 'Laptop',
+                                hasButton: true,
+                              ),
+                              const SizedBox(height: 16),
+                            ],
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 16),
-                      HeadingSection(),
-                      BioSection(
-                        title: 'My Bio',
-                        subtitle: 'Write a fun and punchy intro',
-                      ),
-                      const BasicsSection(
-                        title: 'Basics',
-                        subtitle: 'Choose your basics',
-                      ),
-                      const BasicsSection(
-                        title: 'Interests',
-                        subtitle: 'Choose your interests',
-                      ),
-                      BioSection(
-                        title: 'Languages I know',
-                        subtitle: 'Choose the languages you know',
-                      ),
-                      const SizedBox(height: 100),
-                    ],
+                        const SizedBox(height: 16),
+                        HeadingSection(),
+                        BioSection(
+                          title: 'My Bio',
+                          subtitle: 'Write a fun and punchy intro',
+                        ),
+                        BasicsSection(
+                          title: 'Basics',
+                          subtitle: 'Choose your basics',
+                        ),
+                        BasicsSection(
+                          title: 'Interests',
+                          subtitle: 'Choose your interests',
+                        ),
+                        BioSection(
+                          title: 'Languages I know',
+                          subtitle: 'Choose the languages you know',
+                        ),
+                        const SizedBox(height: 100),
+                      ],
+                    ),
                   );
                 }, childCount: 1),
               ),
@@ -252,7 +324,7 @@ class _HomePageState extends State<HomePage>
                           child: Icon(
                             Icons.favorite,
                             size: 200,
-                            color: Colors.black,
+                            color: Colors.red,
                           ),
                         ),
                       ),
@@ -270,35 +342,26 @@ class _HomePageState extends State<HomePage>
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             FloatingActionButton(
-              heroTag: 'close',
-              onPressed: () {},
+              heroTag: 'dislike',
+              onPressed: _triggerDislikeAction,
               backgroundColor: Colors.black,
               tooltip: 'Dislike',
-              child: Center(
-                child: Transform.rotate(
-                  angle: -180 * 3.1416 / 180, // Rotate by -180 degrees
-                  child: const Icon(
-                    Icons.close, // Close (cross) icon
-                    size: 27.36,
-                    color: Colors.white,
-                  ),
-                ),
+              child: const Icon(
+                Icons.close,
+                size: 27.36,
+                color: Colors.white,
               ),
             ),
-            Expanded(child: Container()),
+            const Spacer(),
             FloatingActionButton(
               heroTag: 'like',
-              onPressed: () {
-                _triggerHeartAnimation();
-              },
+              onPressed: _triggerHeartAnimation,
               backgroundColor: Colors.black,
               tooltip: 'Send a like',
-              child: const Center(
-                child: Icon(
-                  Icons.favorite_outline,
-                  size: 27.36,
-                  color: Colors.green,
-                ),
+              child: const Icon(
+                Icons.favorite_outline,
+                size: 27.36,
+                color: Colors.green,
               ),
             ),
           ],
