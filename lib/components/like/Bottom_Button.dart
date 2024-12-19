9a28ec2da9e27066047372b2c264fc45fed8b796
Ayush Dhiman <ayushd175@gmail.com>
2024-12-19 20:55:50 +0530

    chat integration

--- lib/components/like/Bottom_Button.dart
+++ lib/components/like/Bottom_Button.dart
@@ -1,6 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class BottomButton extends StatelessWidget {
+class BottomButton extends StatefulWidget {
+  final String id;
+
+  const BottomButton({super.key, required this.id});
+
+  @override
+  State<BottomButton> createState() => _BottomButtonState();
+}
+
+class _BottomButtonState extends State<BottomButton> {
+  // / Function to send data to the API
+  Future<void> handleLike() async {
+    const String baseUrl = "http://10.0.2.2:8000"; // Replace with your base URL
+
+    // Prepare the payload
+    Map<String, String> data = {
+      "user1_id": widget.id,
+      "user2_id": "3ab787ad-9a9a-4a52-a0c2-fd06fbd6745c"
+    };
+    // Send POST request
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/chat/conversations/create'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(data),
+      );
+
+      if (response.statusCode == 201) {
+        final Map<String, dynamic> responseBody = jsonDecode(response.body);
+        final String conversationId = responseBody['data']['conversation_id'];
+
+        print(conversationId + '-----conversationId----');
+        // Handle successful response
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Conversation created successfully'),
+        ));
+        Navigator.pushNamed(context, '/chat/message/$conversationId');
+      } else {
+        // Handle failure response
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Failed to start conversation'),
+        ));
+      }
+    } catch (error) {
+      // Handle error
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Error: $error'),
+      ));
+    }
+  }
+
+  void handleNotForMePress() {
+    // Your logic for "Not For Me" button press
+    print("Not For Me button pressed for ID: ${widget.id}");
+  }
+
+  void handleLikePress() {
+    // Your logic for "Like" button press
+    handleLike();
+    print("Like button pressed for ID: ${widget.id}");
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -10,73 +73,76 @@ class BottomButton extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start, // Align buttons to the left
         children: [
-          
           Expanded(
-            child: Container(
-              width: screenWidth,
-              height: 52.0,
-              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 10.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4.0), // Fully rounded corners for all sides
-                border: Border.all(color: Colors.black, width: 1),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center, // Center content horizontally
-                children: [
-                  Image.asset(
-                    'images/cross.png', // Ensure image path is correct
-                    width: 18.0,
-                    height: 18.0,
-                  ),
-                  SizedBox(width: 8.0),
-                  Text(
-                    "Not For Me",
-                    style: TextStyle(
-                      fontFamily: 'NoirPro',
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w400,
-                      letterSpacing: 0.02,
-                      color: Colors.black, // Set text color to black
+            child: GestureDetector(
+              onTap: handleNotForMePress,
+              child: Container(
+                width: screenWidth,
+                height: 52.0,
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4.0),
+                  border: Border.all(color: Colors.black, width: 1),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'images/cross.png',
+                      width: 18.0,
+                      height: 18.0,
                     ),
-                  ),
-                ],
+                    SizedBox(width: 8.0),
+                    Text(
+                      "Not For Me",
+                      style: TextStyle(
+                        fontFamily: 'NoirPro',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 0.02,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-
-          
-          SizedBox(width: 10), 
-
-          
+          SizedBox(width: 10),
           Expanded(
-            child: Container(
-              width: screenWidth,
-              height: 52.0,
-              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 10.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4.0), // Fully rounded corners for both sides
-                color: Colors.black, // Black background for "Like" button
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center, // Center content horizontally
-                children: [
-                  Image.asset(
-                    'images/heart.png', // Ensure image path is correct
-                    width: 24.0,
-                    height: 24.0,
-                  ),
-                  SizedBox(width: 8.0),
-                  Text(
-                    "Like",
-                    style: TextStyle(
-                      fontFamily: 'NoirPro',
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.02,
-                      color: Colors.white, // Set text color to white
+            child: GestureDetector(
+              onTap: handleLikePress,
+              child: Container(
+                width: screenWidth,
+                height: 52.0,
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4.0),
+                  color: Colors.black,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'images/heart.png',
+                      width: 24.0,
+                      height: 24.0,
                     ),
-                  ),
-                ],
+                    SizedBox(width: 8.0),
+                    Text(
+                      "Like",
+                      style: TextStyle(
+                        fontFamily: 'NoirPro',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.02,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
--- lib/src/ui/chat/message.dart
+++ lib/src/ui/chat/message.dart
@@ -1,8 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/status.dart' as status;
+import 'dart:convert';
 import 'package:testapp/components/chat/chat_header.dart';
 import 'package:testapp/components/chat/chatbox.dart';
+import 'package:intl/intl.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  final String conversationId;
+  final String currentUserId;
+  final String recipientUserId;
+
+  ChatScreen({
+    required this.conversationId,
+    required this.currentUserId,
+    required this.recipientUserId,
+  });
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  late WebSocketChannel _channel;
+  final List<Map<String, String>> _messages = [];
+  final TextEditingController _messageController = TextEditingController();
+
+  String formatTimestamp(String timestamp) {
+    // Parse the ISO 8601 timestamp
+    DateTime dateTime = DateTime.parse(timestamp);
+
+    // Format the date and time for display
+    String formattedTime =
+        DateFormat('hh:mm a').format(dateTime); // e.g., 04:42 PM
+    String formattedDate =
+        DateFormat('MMM d, yyyy').format(dateTime); // e.g., Dec 18, 2024
+
+    // Check if the message is from today
+    DateTime now = DateTime.now();
+    if (dateTime.day == now.day &&
+        dateTime.month == now.month &&
+        dateTime.year == now.year) {
+      return formattedTime; // Show only the time if the message is from today
+    }
+
+    return '$formattedDate, $formattedTime'; // Show date and time for older messages
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize WebSocket channel
+    _channel = WebSocketChannel.connect(
+      Uri.parse('ws://10.0.2.2:8000/ws/chat/${widget.conversationId}'),
+    );
+
+    // Listen for incoming messages
+    _channel.stream.listen(
+      (event) {
+        final data = jsonDecode(event);
+        setState(() {
+          _messages.add({
+            'sender_id': data['sender_id'],
+            'content': data['content'],
+            'timestamp': data['timestamp'],
+          });
+        });
+      },
+      onError: (error) {
+        print('WebSocket Error: $error');
+      },
+      onDone: () {
+        print('WebSocket connection closed.');
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _channel.sink.close(status.goingAway); // Close WebSocket connection
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void _sendMessage() {
+    final message = _messageController.text.trim();
+    if (message.isNotEmpty) {
+      final jsonMessage = jsonEncode({
+        'sender_id': widget.currentUserId,
+        'content': message,
+        'recipient_id': widget.recipientUserId,
+      });
+
+      _channel.sink.add(jsonMessage);
+
+      setState(() {
+        _messages.add({
+          'sender_id': widget.currentUserId,
+          'content': message,
+          'timestamp': DateTime.now().toString(),
+        });
+      });
+
+      _messageController.clear();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,247 +123,110 @@ class ChatScreen extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  MessageScreen(), // Merged MessageScreen content
+                  MessageScreen(
+                      messages: _messages, currentUserId: widget.currentUserId),
                 ],
               ),
             ),
           ),
 
-          ChatBox(),
+          ChatBox(
+              sendMessage: _sendMessage, messageController: _messageController),
         ],
       ),
     );
   }
 }
 
-// MessageScreen Component
+// MessageScreen Component (Dynamic)
 class MessageScreen extends StatelessWidget {
+  final List<Map<String, String>> messages;
+  final String currentUserId;
+
+  MessageScreen({required this.messages, required this.currentUserId});
+
+  String formatTimestamp(String timestamp) {
+    // Parse the ISO 8601 timestamp
+    DateTime dateTime = DateTime.parse(timestamp);
+
+    // Format the date and time for display
+    String formattedTime =
+        DateFormat('hh:mm a').format(dateTime); // e.g., 04:42 PM
+    String formattedDate =
+        DateFormat('MMM d, yyyy').format(dateTime); // e.g., Dec 18, 2024
+
+    // Check if the message is from today
+    DateTime now = DateTime.now();
+    if (dateTime.day == now.day &&
+        dateTime.month == now.month &&
+        dateTime.year == now.year) {
+      return formattedTime; // Show only the time if the message is from today
+    }
+
+    return '$formattedDate, $formattedTime'; // Show date and time for older messages
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // First message frame
-          Container(
-            width: 311,
-            padding: const EdgeInsets.all(16.0),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(24),
-                topRight: Radius.circular(16),
-                bottomRight: Radius.circular(16),
-                bottomLeft: Radius.circular(4),
-              ),
-            ),
-            child: Text(
-              "Lorem ipsum dolor sit amet consectetur. Est molestie cras quis commodo enim enim iaculis. Fermentum augue aliquet lorem enim sem egestas tristique.",
-              style: TextStyle(
-                fontFamily: 'NoirPro',
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                letterSpacing: 0.02,
-                color: Colors.black.withOpacity(0.6),
-                height: 1.5,
-              ),
-            ),
-          ),
-          // Timestamp outside the message frame
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Text(
-              "10:25",
-              style: TextStyle(
-                fontFamily: 'Helvetica Neue',
-                fontSize: 12,
-                fontWeight: FontWeight.w500,
-                color: Colors.black.withOpacity(0.3),
-                letterSpacing: 0.02,
-              ),
-            ),
-          ),
-          const SizedBox(height: 24),
-          Container(
-            width: 311,
-            padding: const EdgeInsets.all(16.0),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(24),
-                topRight: Radius.circular(16),
-                bottomRight: Radius.circular(16),
-                bottomLeft: Radius.circular(4),
-              ),
-            ),
-            child: Text(
-              "Lorem ipsum dolor sit amet consectetur. Est molestie cras quis commodo enim enim iaculis. Fermentum augue aliquet lorem enim sem egestas tristique.",
-              style: TextStyle(
-                fontFamily: 'NoirPro',
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                letterSpacing: 0.02,
-                color: Colors.black.withOpacity(0.6),
-                height: 1.5,
-              ),
-            ),
-          ),
-          // Timestamp outside the message frame
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Text(
-              "10:25",
-              style: TextStyle(
-                fontFamily: 'Helvetica Neue',
-                fontSize: 12,
-                fontWeight: FontWeight.w500,
-                color: Colors.black.withOpacity(0.3),
-                letterSpacing: 0.02,
-              ),
-            ),
-          ),
-          const SizedBox(height: 24),
-          Container(
-            width: 311,
-            padding: const EdgeInsets.all(16.0),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(24),
-                topRight: Radius.circular(16),
-                bottomRight: Radius.circular(16),
-                bottomLeft: Radius.circular(4),
-              ),
-            ),
-            child: Text(
-              "Lorem ipsum dolor sit amet consectetur. Est molestie cras quis commodo enim enim iaculis. Fermentum augue aliquet lorem enim sem egestas tristique.",
-              style: TextStyle(
-                fontFamily: 'NoirPro',
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                letterSpacing: 0.02,
-                color: Colors.black.withOpacity(0.6),
-                height: 1.5,
-              ),
-            ),
-          ),
-          // Timestamp outside the message frame
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Text(
-              "10:25",
-              style: TextStyle(
-                fontFamily: 'Helvetica Neue',
-                fontSize: 12,
-                fontWeight: FontWeight.w500,
-                color: Colors.black.withOpacity(0.3),
-                letterSpacing: 0.02,
-              ),
-            ),
-          ),
-          const SizedBox(height: 24),
-
-          // Centered "2:30 PM" time frame
-          Center(
-            child: Container(
-              width: 74,
-              height: 22,
-              decoration: BoxDecoration(
-                color: Color(0xFFD1D1D6),
-                borderRadius: BorderRadius.circular(78),
-              ),
-              child: const Center(
+        children: messages.map((message) {
+          final isSentByCurrentUser = message['sender_id'] == currentUserId;
+          final formattedTS = formatTimestamp(message['timestamp'] ?? '');
+          return Column(
+            crossAxisAlignment: isSentByCurrentUser
+                ? CrossAxisAlignment.end
+                : CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 311,
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color:
+                      isSentByCurrentUser ? Colors.blue[100] : Colors.grey[200],
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    topRight: Radius.circular(16),
+                    bottomRight: isSentByCurrentUser
+                        ? Radius.circular(4)
+                        : Radius.circular(16),
+                    bottomLeft: isSentByCurrentUser
+                        ? Radius.circular(16)
+                        : Radius.circular(4),
+                  ),
+                ),
                 child: Text(
-                  "2 : 30 PM",
+                  message['content'] ?? '',
+                  style: TextStyle(
+                    fontFamily: 'NoirPro',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: 0.02,
+                    color: Colors.black.withOpacity(0.6),
+                    height: 1.5,
+                  ),
+                ),
+              ),
+              // Timestamp placeholder (for now)
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  formattedTS,
                   style: TextStyle(
                     fontFamily: 'Helvetica Neue',
                     fontSize: 12,
                     fontWeight: FontWeight.w500,
-                    color: Colors.white,
+                    color: Colors.black.withOpacity(0.3),
                     letterSpacing: 0.02,
                   ),
                 ),
               ),
-            ),
-          ),
-          const SizedBox(height: 24),
-
-          // Second message frame
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(16.0),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(16),
-                topRight: Radius.circular(24),
-                bottomRight: Radius.circular(4),
-                bottomLeft: Radius.circular(16),
-              ),
-            ),
-            child: Text(
-              "Lorem ipsum dolor sit amet consectetur. Est molestie cras quis commodo enim enim iaculis.",
-              style: TextStyle(
-                fontFamily: 'NoirPro',
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                letterSpacing: 0.02,
-                color: Colors.black.withOpacity(0.6),
-                height: 1.5,
-              ),
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(16.0),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(16),
-                topRight: Radius.circular(24),
-                bottomRight: Radius.circular(4),
-                bottomLeft: Radius.circular(16),
-              ),
-            ),
-            child: Text(
-              "Lorem ipsum dolor sit amet consectetur. Est molestie cras quis commodo enim enim iaculis.",
-              style: TextStyle(
-                fontFamily: 'NoirPro',
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                letterSpacing: 0.02,
-                color: Colors.black.withOpacity(0.6),
-                height: 1.5,
-              ),
-            ),
-          ),
-
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(16.0),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(16),
-                topRight: Radius.circular(24),
-                bottomRight: Radius.circular(4),
-                bottomLeft: Radius.circular(16),
-              ),
-            ),
-            child: Text(
-              "Lorem ipsum dolor sit amet consectetur. Est molestie cras quis commodo enim enim iaculis.",
-              style: TextStyle(
-                fontFamily: 'NoirPro',
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                letterSpacing: 0.02,
-                color: Colors.black.withOpacity(0.6),
-                height: 1.5,
-              ),
-            ),
-          ),
-        ],
+              const SizedBox(height: 24),
+            ],
+          );
+        }).toList(),
       ),
     );
   }
